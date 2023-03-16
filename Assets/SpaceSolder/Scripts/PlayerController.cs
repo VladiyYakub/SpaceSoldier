4250119ff460867d9ce41f63ad2eb16ce92fb06f
Vladyslav Yakubenko <vladikyakubenko@gmail.com>
2023-03-16 17:41:13 +0200

    Add Raycast

--- Assets/SpaceSolder/Scripts/PlayerController.cs
+++ Assets/SpaceSolder/Scripts/PlayerController.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private FixedJoystick _joystickMove;
-    [SerializeField] private FixedJoystick _joystickLook;
+    //[SerializeField] private FixedJoystick _joystickLook;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _movespeed;
 
@@ -13,24 +12,24 @@ public class PlayerController : MonoBehaviour
     {
         _rb.velocity = new Vector3(_joystickMove.Horizontal * _movespeed, _rb.velocity.y, _joystickMove.Vertical * _movespeed);
 
-        //        if (_joystickLook.Horizontal != 0 || _joystickLook.Vertical != 0)
-        //        {
-        //            transform.rotation = Quaternion.LookRotation(_rb.velocity);
-        //            _animator.SetBool("isRunning", true);
-        //        }
-        //        else
-        //        {
-        //            _animator.SetBool("isRunning", false);
-        //        }
+        if (_joystickMove.Horizontal != 0 || _joystickMove.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rb.velocity);
+            _animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
     }
 
     //[SerializeField] public FixedJoystick _moveJoystick;
     //[SerializeField] private Rigidbody _rb;
     //[SerializeField] private Animator _animation;
-    ////[SerializeField] private float _speed;
+    //[SerializeField] private float _speed;
     //private void Update()
     //{
-    //    float horiz = _moveJoystick.Horizontal; 
+    //    float horiz = _moveJoystick.Horizontal;
     //    float vert = _moveJoystick.Vertical;
     //    Vector3 direction = new Vector3(horiz, 0, vert).normalized;
     //    transform.Translate(direction * 0.02f, Space.World);
