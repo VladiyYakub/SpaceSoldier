a164756eae4ed12e487711c5753fa7eff71550b3
Vladyslav Yakubenko <vladikyakubenko@gmail.com>
2023-04-03 14:46:25 +0300

    Sorting

---
+++ Assets/SpaceSolder/Dev/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] private Button _shootButton;
+    [SerializeField] private Weapon _weapon;
+
+    private float lastShotTime;
+
+    private void Awake()
+    {
+        if (_shootButton)
+            _shootButton.onClick.AddListener(Shoot);
+    }
+
+    private void Shoot()
+    {
+        if (Time.time - lastShotTime < _weapon.FireRate)
+            return;
+
+        lastShotTime = Time.time;
+        _weapon.Shoot();
+    }
+}
